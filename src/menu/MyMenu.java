39b5b73b764dc7a077804af0111789e7bb6cb8ec
VirginieNar <virginielamesse23@gmail.com>
2020-02-20 16:01:19 +0000

    hello

--- src/menu/MyMenu.java
+++ src/menu/MyMenu.java
@@ -1,4 +1,14 @@
 package menu;
 
+import javax.swing.*;
+import  static java.lang.System.out;
+
 public class MyMenu {
+
+    public static void main(String[] args) {
+        out.println("Virginie Lamesse Nar");
+        String var_test = new String("Comment va");
+        var_test.contains("va");
+        // write your code here
+    }
 }
